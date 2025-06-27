8fb60140a84753f90403a735c2b3b6bb2295a664
Kenny <kennykotten@gmail.com>
2025-06-26 21:24:43 -0700

    add Enviornment

--- src/com/craftinginterpreters/lox/Parser.java
+++ src/com/craftinginterpreters/lox/Parser.java
@@ -20,18 +20,23 @@ class Parser {
 	List<Stmt> parse() {
 		List<Stmt> statements = new ArrayList<>();
 		while (!isAtEnd()) {
-			statements.add(statement());
+			statements.add(declaration());
 		}
 
 		return statements;
 	}
-//	Expr parse() {
-//		try {
-//			return expression();
-//		} catch (ParseError error) {
-//			return null;
-//		}
-//	}
+
+	private Stmt declaration() {
+		try {
+			if (match(VAR))
+				return varDeclaration();
+
+			return statement();
+		} catch (ParseError error) {
+			synchronize();
+			return null;
+		}
+	}
 
 	private Expr expression() {
 		return equality();
@@ -48,6 +53,18 @@ class Parser {
 		Expr value = expression();
 		consume(SEMICOLON, "Expect ';' after value.");
 		return new Stmt.Print(value);
+	}
+
+	private Stmt varDeclaration() {
+		Token name = consume(IDENTIFIER, "Expect variable name.");
+
+		Expr initializer = null;
+		if (match(EQUAL)) {
+			initializer = expression();
+		}
+
+		consume(SEMICOLON, "Expect ';' after variable declaration.");
+		return new Stmt.Var(name, initializer);
 	}
 
 	private Stmt expressionStatement() {
@@ -123,6 +140,9 @@ class Parser {
 
 		if (match(NUMBER, STRING)) {
 			return new Expr.Literal(previous().literal);
+		}
+		if (match(IDENTIFIER)) {
+			return new Expr.Variable(previous());
 		}
 
 		if (match(LEFT_PAREN)) {
--- src/com/craftinginterpreters/tool/GenerateAst.java
+++ src/com/craftinginterpreters/tool/GenerateAst.java
@@ -6,17 +6,26 @@ import java.util.Arrays;
 import java.util.List;
 
 public class GenerateAst {
-	public static void main(String[] args) throws IOException {
-		if (args.length != 1) {
-			System.err.println("Usage: generate_ast <output directory>");
-			System.exit(64);
-		}
-		String outputDir = args[0];
-		defineAst(outputDir, "Expr", Arrays.asList("Binary   : Expr left, Token operator, Expr right",
-				"Grouping : Expr expression", "Literal  : Object value", "Unary    : Token operator, Expr right"));
-
-		defineAst(outputDir, "Stmt", Arrays.asList("Expression : Expr expression", "Print      : Expr expression"));
+public static void main(String[] args) throws IOException {
+	if (args.length != 1) {
+		System.err.println("Usage: generate_ast <output directory>");
+		System.exit(64);
 	}
+	String outputDir = args[0];
+	defineAst(outputDir, "Expr", Arrays.asList(
+		"Binary   : Expr left, Token operator, Expr right",
+		"Grouping : Expr expression",
+		"Literal  : Object value",
+		"Unary    : Token operator, Expr right",
+		"Variable : Token name"
+	));
+
+	defineAst(outputDir, "Stmt", Arrays.asList(
+		"Expression : Expr expression",
+		"Print      : Expr expression",
+		"Var        : Token name, Expr initializer"
+	));
+}
 
 	private static void defineAst(String outputDir, String baseName, List<String> types) throws IOException {
 		String path = outputDir + "/" + baseName + ".java";
