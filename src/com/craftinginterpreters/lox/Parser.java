006a028412b662c1e917f5443d89d53dd3cfcea0
Kenny <kennykotten@gmail.com>
2025-06-27 14:51:10 -0700

    add: local vars, nesting, shadowing

--- src/com/craftinginterpreters/lox/Parser.java
+++ src/com/craftinginterpreters/lox/Parser.java
@@ -39,12 +39,14 @@ class Parser {
 	}
 
 	private Expr expression() {
-		return equality();
+		return assignment();
 	}
 
 	private Stmt statement() {
 		if (match(PRINT))
 			return printStatement();
+		if (match(LEFT_BRACE))
+			return new Stmt.Block(block());
 
 		return expressionStatement();
 	}
@@ -53,6 +55,17 @@ class Parser {
 		Expr value = expression();
 		consume(SEMICOLON, "Expect ';' after value.");
 		return new Stmt.Print(value);
+	}
+
+	private List<Stmt> block() {
+		List<Stmt> statements = new ArrayList<>();
+
+		while (!check(RIGHT_BRACE) && !isAtEnd()) {
+			statements.add(declaration());
+		}
+
+		consume(RIGHT_BRACE, "Expect '}' after block.");
+		return statements;
 	}
 
 	private Stmt varDeclaration() {
@@ -71,6 +84,24 @@ class Parser {
 		Expr expr = expression();
 		consume(SEMICOLON, "Expect ';' after expression.");
 		return new Stmt.Expression(expr);
+	}
+
+	private Expr assignment() {
+		Expr expr = equality();
+
+		if (match(EQUAL)) {
+			Token equals = previous();
+			Expr value = assignment();
+
+			if (expr instanceof Expr.Variable) {
+				Token name = ((Expr.Variable) expr).name;
+				return new Expr.Assign(name, value);
+			}
+
+			error(equals, "Invalid assignment target.");
+		}
+
+		return expr;
 	}
 
 	private Expr equality() {
